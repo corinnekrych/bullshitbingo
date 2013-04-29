0fc098653ac68df21f56572f1bb5a655894fc461
corinnekrych <corinnekrych@gmail.com>
2013-04-29 10:05:40 +0200

    version upgrade Grails

--- web-app/js/bingo/configuration-bootstrap.js
+++ web-app/js/bingo/configuration-bootstrap.js
@@ -2,11 +2,11 @@ var bingo = bingo || {};
 
 bingo.loadConfiguration = (function () {
     bingo.configuration = {
-        baseURL: "http://localhost:8080/bullshitbingo/",
-        applicationContext: "bullshitbingo/",
+        //baseURL: "http://localhost:8080/bullshitbingo/",
+        //applicationContext: "bullshitbingo/",
         //Uncomment before pushing to cloudfoundry
-        //baseURL: "http://bullshitbingo.cloudfoundry.com/",
-        //applicationContext: "",
+        baseURL: "http://bullshitbingo.cloudfoundry.com/",
+        applicationContext: "",
         namespace: "bingo",
         domain:[]
     };
